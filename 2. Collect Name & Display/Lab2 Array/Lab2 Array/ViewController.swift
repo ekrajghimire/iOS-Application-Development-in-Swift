33bf833e9e4129b3a45335b4d686aa77113ac7e0
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-11-23 15:22:47 +0530

    Add files via upload

---
+++ 2. Collect Name & Display/Lab2 Array/Lab2 Array/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Lab2 Array
+//
+//  Created by Ekraj Ghimire on 11/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var arr1:[String]=[]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        arr1 = ["Ekraj","Manish","Manju","Full Name"]
+        print("\(arr1)")
+    }
+
+
+}
+
